7fdc9ea9607edf21b65c775e600ab3a930eb41c2
noje <nojy99wodnjs@gmail.com>
2021-05-02 20:25:57 +0900

    Set URL sturcture

---
+++ mindnote/diary/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('info/',views.info),
+    # path('write/',views.page_create),
+    # path('page/<int:page_id>/',views.page_detail),
+    # path('page/<int:page_id>/edit/',views.page_update),
+    # path('page/<int:page_id>/delete',views.page_delete),
+]
