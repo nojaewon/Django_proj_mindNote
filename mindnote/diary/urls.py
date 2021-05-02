8c706d19ba46083c52bedbe0b77ddbe0a0e8d3f2
noje <nojy99wodnjs@gmail.com>
2021-05-02 20:33:17 +0900

    Add urlpattern 'page_list'

--- mindnote/diary/urls.py
+++ mindnote/diary/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path('',views.page_list),
     # path('info/',views.info),
     # path('write/',views.page_create),
     # path('page/<int:page_id>/',views.page_detail),
