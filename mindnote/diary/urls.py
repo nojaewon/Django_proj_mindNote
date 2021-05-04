5b006c6431c9f22f42328b31787141f4b1f7ada9
noje <nojy99wodnjs@gmail.com>
2021-05-04 12:17:39 +0900

    make URLs' name

--- mindnote/diary/urls.py
+++ mindnote/diary/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.page_list, name = 'page-list'),
-    # path('info/',views.info),
-    # path('write/',views.page_create),
+    # path('info/',views.info, name = 'info'),
+    # path('write/',views.page_create, name = 'page-create'),
     path('page/<int:page_id>/',views.page_detail, name = 'page-detail'),
-    # path('page/<int:page_id>/edit/',views.page_update),
-    # path('page/<int:page_id>/delete',views.page_delete),
+    # path('page/<int:page_id>/edit/',views.page_update, name = 'page-update'),
+    # path('page/<int:page_id>/delete',views.page_delete, name = 'page-delete'),
 ]
