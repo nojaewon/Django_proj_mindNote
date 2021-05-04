c61fb8ad0c6b5cf813f9b7468d4ca00110664a9b
noje <nojy99wodnjs@gmail.com>
2021-05-04 12:13:34 +0900

    Link 'page_list' and 'page_detail' by URL

--- mindnote/diary/urls.py
+++ mindnote/diary/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.page_list),
+    path('',views.page_list, name = 'page-list'),
     # path('info/',views.info),
     # path('write/',views.page_create),
-    # path('page/<int:page_id>/',views.page_detail),
+    path('page/<int:page_id>/',views.page_detail, name = 'page-detail'),
     # path('page/<int:page_id>/edit/',views.page_update),
     # path('page/<int:page_id>/delete',views.page_delete),
 ]
--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -6,3 +6,8 @@ def page_list(request):
     object_list = Page.objects.all()
     context = {'object_list':object_list}
     return render(request, 'diary/page_list.html',context)
+
+def page_detail(request,page_id):
+    page_obj = Page.objects.get(id=page_id)
+    context = {'object' : page_obj}
+    return render(request, 'diary/page_detail.html',context)
